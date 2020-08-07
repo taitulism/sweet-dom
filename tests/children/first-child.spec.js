d70f835d2c88583531a0504a2a778152aefa1eb9
Itai Tenenbaum <taitulism@gmail.com>
2020-08-07 14:42:12 +0300

    add setter to first & last child

--- tests/children/first-child.spec.js
+++ tests/children/first-child.spec.js
@@ -10,30 +10,57 @@ const setWinDoc = (dom) => {
 };
 
 describe('firstChild', () => {
+	let parent;
 	beforeEach(() => JSDOM.fromFile('./tests/children/children.html').then((dom) => {
 		setWinDoc(dom);
+		parent = document.getElementById('parent');
 	}));
 
 	afterEach(() => {
 		global.window.close();
 		global.window = null;
 		global.document = null;
+		parent = null;
 	});
 
 	it('is a function', () => expect(firstChild).to.be.a('function'));
 
 	it('gets element\'s first child - html elements only', () => {
-		const elm = document.getElementById('parent');
-		expect(firstChild(elm).textContent).to.equal('A');
+		expect(firstChild(parent).textContent).to.equal('A');
 	});
 
 	it('gets element\'s first child - with nodes (text, comments etc.)', () => {
-		const elm = document.getElementById('parent');
-		expect(firstChild(elm, true).textContent).to.contain('\n\t\t');
+		expect(firstChild(parent, true).textContent).to.contain('\n\t\t');
 	});
 
 	it('returns `null` when element has no children', () => {
-		const elm = document.getElementById('empty');
-		expect(firstChild(elm, false)).to.be.null;
+		const emptyElm = document.getElementById('empty');
+		expect(firstChild(emptyElm, false)).to.be.null;
+	});
+
+	it('sets the first child of an element', () => {
+		const newElm = document.createElement('div');
+		newElm.id = 'new-elm';
+
+		expect(parent.children).to.have.lengthOf(4);
+		expect(parent.children[0].textContent).to.equal('A');
+
+		firstChild(parent, newElm);
+
+		expect(parent.children[0].id).to.equal('new-elm');
+		expect(parent.children).to.have.lengthOf(5);
+	});
+
+	it('sets the first child of an element - with text nodes', () => {
+		const newElm = document.createElement('div');
+		newElm.id = 'new-elm';
+
+		expect(parent.childNodes).to.have.lengthOf(11);
+		expect(parent.childNodes[0].textContent).to.contain('\n\t\t');
+
+		firstChild(parent, newElm, true);
+
+		expect(parent.childNodes[0].id).to.equal('new-elm');
+		expect(parent.childNodes).to.have.lengthOf(12);
 	});
 });
