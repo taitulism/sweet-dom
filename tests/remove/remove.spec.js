deea9dadde18f6cacdb711cc9876fef9f05f1b8d
Itai Tenenbaum <taitulism@gmail.com>
2020-08-08 16:28:09 +0300

    remove handle multiple elms

--- tests/remove/remove.spec.js
+++ tests/remove/remove.spec.js
@@ -10,24 +10,34 @@ const setWinDoc = (dom) => {
 };
 
 describe('remove', () => {
+	let parent;
 	beforeEach(() => JSDOM.fromFile('./tests/remove/remove.html').then((dom) => {
 		setWinDoc(dom);
+		parent = document.getElementById('root');
 	}));
 
 	afterEach(() => {
 		global.window.close();
+		parent = null;
 		global.window = null;
 		global.document = null;
 	});
 
 	it('is a function', () => expect(remove).to.be.a('function'));
 
-	it('remove an element from DOM', () => {
-		const parent = document.getElementById('root');
+	it('removes an element from DOM', () => {
 		const child = document.getElementById('removee');
 
-		expect(parent.innerHTML).to.contain('here');
+		expect(parent.children).to.have.lengthOf(2);
 		remove(child);
-		expect(parent.innerHTML).to.not.contain('here');
+		expect(parent.children).to.have.lengthOf(1);
+	});
+
+	it('removes multiple elements from DOM', () => {
+		const elms = document.getElementsByClassName('removable');
+
+		expect(parent.children).to.have.lengthOf(2);
+		remove(...elms);
+		expect(parent.children).to.have.lengthOf(0);
 	});
 });
